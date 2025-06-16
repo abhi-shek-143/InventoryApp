036f0c016af79c2ad4029450a905ebc5ea5ce291
alfred-nrml <alfrednrml@gmail.com>
2025-06-16 17:46:53 +0530

    Updated [Member 5] Implemented product update logic and validations

--- Services/IProductService.cs
+++ Services/IProductService.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using InventoryApp.Models;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace InventoryApp.Services
 {
-    internal interface IProductService
+    public interface IProductService
     {
+        Product AddProduct(string name, string description, decimal price, int quantity, Guid supplierId);
+        Product UpdateProduct(Product updatedProduct);
+        bool DeleteProduct(Guid productId);
+        List<Product> GetAllProducts();
     }
 }
